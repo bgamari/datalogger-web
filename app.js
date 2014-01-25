ce1863685eb39406f98127ee4aae9941656e0cdd
Laura Dietz <dietz@cs.umass.edu>
2014-01-25 15:21:29 -0500

    Clean up javascript

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+var current_device = null;
+
+function set_acquiring(acquiring) {
+    if (acquiring) {
+        $("#start").addClass("disabled");
+        $("#stop").removeClass("disabled");
+    } else {
+        $("#start").removeClass("disabled");
+        $("#stop").addClass("disabled");
+    }
+}
+
 function start_acquire() {
     $.ajax("/dev/start", { type: "PUT" });
+    set_acquiring(true);
 }
 
 function eject_device(device) {
     $("#device-"+device).remove();
 }
 
+function select_device(device) {
+    current_device = device;
+    // setup UI elements
+}
+
 function refresh_devices() {
     $.ajax("/devices", {
         type: "POST",
+
         success: function(data, status, xhr) {
             $("#devices").empty();
             for (device in data) {
