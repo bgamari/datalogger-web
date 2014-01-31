d00ad965243b21df24dc1d050ffbb54b272217c3
Ben Gamari <bgamari.foss@gmail.com>
2014-01-30 22:18:12 -0500

    A great deal of refactoring

--- WebApp.hs
+++ WebApp.hs
@@ -9,99 +9,36 @@ import Control.Monad (forM_, when, void)
 import Control.Monad.IO.Class
 import Control.Monad.Reader
 import Control.Monad.Trans.Class
-import Data.Time.Clock.POSIX (getPOSIXTime)
 import Control.Concurrent.STM       
 
 import qualified Data.Text.Lazy as TL
-import qualified Data.Map as M
 import qualified Data.Vector as V
 import qualified Data.Csv as Csv
 import Control.Error
 
-import Network.HTTP.Types.Status (status500, status404)
+import Network.HTTP.Types.Status (status200, status202, status500, status404)
 import Data.Aeson (ToJSON(..), FromJSON(..), (.=))
 import qualified Data.Aeson as JSON
 import Web.Scotty.Trans hiding (ScottyM, ActionM)
 
 import DataLogger (DataLogger, DeviceId, Sample)
 import qualified DataLogger as DL
+import DeviceList
 
 deriving instance Parsable DeviceId
-
-newtype DeviceName = DN String
-                   deriving (Show, Eq, Ord, ToJSON, FromJSON, Parsable)
+deriving instance Parsable DeviceName
 
 instance ToJSON DeviceId where
     toJSON (DL.DevId devId) = toJSON devId
 
-data Device = Device { devName   :: DeviceName -- ^ Friendly name
-                     , devPath   :: FilePath   -- ^ Path to serial device
-                     , devLogger :: DataLogger -- ^ @DataLogger@
-                     , devId     :: DeviceId   -- ^ Unique @DeviceId@
-                     , devSamples :: TVar (Maybe (V.Vector Sample))  -- ^ Cached samples
-                     }
-                    
-type DeviceList = TVar (M.Map DeviceId Device)
-type ScottyM = ScottyT TL.Text (ReaderT DeviceList IO)
-type ActionM = ActionT TL.Text (ReaderT DeviceList IO)
-
-instance (ScottyError e, Monad m)
-       => MonadReader DeviceList (ActionT e (ReaderT DeviceList m)) where
-    ask = lift ask
-    local f = undefined
-    reader f = lift $ reader f
+type ScottyM = ScottyT TL.Text (DeviceListT IO)
+type ActionM = ActionT TL.Text (DeviceListT IO)
 
 main = do 
-    devices <- newTVarIO M.empty
-    runReaderT refreshDevices devices
-    let run m = runReaderT m devices
+    devList <- newDeviceList
+    let run = runDeviceListT devList
+    run refreshDevices
     scottyT 3000 run run routes
-
-withDeviceList :: (MonadReader DeviceList m, MonadIO m)
-               => (M.Map DeviceId Device -> m a) -> m a
-withDeviceList m = ask >>= liftIO . atomically . readTVar >>= m
-
-filterDevicesByName :: DeviceName -> ReaderT DeviceList IO [Device]
-filterDevicesByName name =
-    withDeviceList $ return . filter (\dev->devName dev == name) . M.elems
-
-lookupDeviceId :: DeviceId -> ReaderT DeviceList IO (Maybe Device)
-lookupDeviceId devId =
-    withDeviceList $ return . M.lookup devId
-    
-refreshDevices :: ReaderT DeviceList IO ()
-refreshDevices = do
-    loggers <- liftIO DL.findDataLoggers
-    devList <- ask
-    dls <- liftIO $ atomically $ readTVar devList
-    let knownDevices = M.elems dls
-    
-    -- Make sure no devices have disappeared
-    forM_ (M.assocs dls) $ \(devId, dev)->do
-        when (devPath dev `notElem` loggers)
-          $ liftIO $ atomically $ modifyTVar devList $ M.delete devId
-
-    -- Add new devices
-    let newDevs = filter (`notElem` map devPath knownDevices) loggers
-    forM_ newDevs $ \devPath->runEitherT $ addDevice devPath
-
-addDevice :: MonadIO m
-          => FilePath -> EitherT String (ReaderT DeviceList m) DeviceId
-addDevice devPath = do
-    dl <- DL.open devPath
-    EitherT $ liftIO $ runEitherT $ checkRTCTime dl
-    devList <- lift ask
-    devId <- DL.getDeviceId dl
-    name <- DL.get dl DL.deviceName
-    samples <- liftIO $ newTVarIO Nothing
-    liftIO $ atomically $ modifyTVar devList
-           $ M.insert devId $ Device { devPath    = devPath
-                                     , devLogger  = dl
-                                     , devId      = devId
-                                     , devName    = DN name
-                                     , devSamples = samples
-                                     }
-    return devId
 
 withDevice :: (Device -> ActionM ()) -> ActionM ()
 withDevice action = do
@@ -156,17 +93,6 @@ instance ToJSON DL.Sample where
                   , "value"  .= DL.sampleValue s
                   ]
 
-fetchSamples :: Device -> EitherT String IO (V.Vector Sample)
-fetchSamples dev = do
-    cache <- liftIO $ atomically $ readTVar (devSamples dev)
-    case cache of
-      Just samples -> return samples
-      Nothing -> do
-        count <- DL.getSampleCount (devLogger dev)
-        samples <- V.fromList <$> DL.getSamples (devLogger dev) 0 count
-        liftIO $ atomically $ writeTVar (devSamples dev) (Just samples)
-        return samples
-
 csv :: Csv.ToRecord a => [a] -> ActionM ()
 csv xs = do
     setHeader "Content-Type" "text/plain"
@@ -174,17 +100,28 @@ csv xs = do
 
 routes :: ScottyM () 
 routes = do
+    get "/devices" $ do
+        lift getDeviceList >>= json . map devId
+    post "/devices" $ do
+        lift refreshDevices
+        lift getDeviceList >>= json . map devId
+
     getPutSetting "acquiring" DL.acquiring
     getPutSetting "sample-period" DL.samplePeriod
     getPutSetting "rtc-time" DL.rtcTime
     getPutNVSetting "acquire-on-boot" DL.acquireOnBoot
     getPutNVSetting "name" DL.deviceName
+    
+    post "/devices/:device/erase" $ withDevice $ \dev->do
+        result <- liftIO $ runEitherT $ DL.resetSampleCount (devLogger dev)
+        case result of
+          Right ()     -> status status200
+          Left error   -> do html "<h1>Error fetching sample count</h1>"
+                             status status500
 
-    get "/devices" $ do
-        withDeviceList $ json . M.keys
-    post "/devices" $ do
-        lift refreshDevices
-        withDeviceList $ json . M.keys
+    post "/devices/:device/eject" $ withDevice $ \dev->do
+        -- TODO
+        status status200
         
     get "/devices/:device/sample_count" $ withDevice $ \dev->do
         result <- liftIO $ runEitherT $ DL.getSampleCount (devLogger dev)
@@ -194,18 +131,20 @@ routes = do
                              status status500
                              
     get "/devices/:device/samples/csv" $ withDevice $ \dev->do
-        result <- liftIO $ runEitherT $ fetchSamples dev
+        result <- lift $ runEitherT $ fetch dev
         case result of 
-          Right samples -> csv (V.toList samples)
-          Left error    -> do html "<h1>Error fetching samples</h1>"
-                              status status500
+          Right (Right samples) -> csv (V.toList samples)
+          Right (Left progress) -> status status202
+          Left error            -> do html "<h1>Error fetching samples</h1>"
+                                      status status500
 
     get "/devices/:device/samples/json" $ withDevice $ \dev->do
-        result <- liftIO $ runEitherT $ fetchSamples dev
+        result <- lift $ runEitherT $ fetch dev
         case result of 
-          Right samples -> json samples
-          Left error    -> do html "<h1>Error fetching samples</h1>"
-                              status status500
+          Right (Right samples) -> json samples
+          Right (Left progress) -> status status202
+          Left error            -> do html "<h1>Error fetching samples</h1>"
+                                      status status500
 
     get "/" $ file "index.html"
     get "/jquery.js" $ file "jquery-2.0.3.js"
@@ -215,10 +154,3 @@ routes = do
     get "/chart.css" $ file "chart.css"
     get "/chart.js" $ file "chart.js"
 
--- | Check that the RTC time has been set    
-checkRTCTime :: DataLogger -> EitherT String IO ()
-checkRTCTime dl = do
-    t <- DL.get dl DL.rtcTime
-    when (t == 0) $ do
-      realTime <- liftIO getPOSIXTime
-      DL.set dl DL.rtcTime (round $ realToFrac realTime)
